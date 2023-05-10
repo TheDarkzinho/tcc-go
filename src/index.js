019ebbea31878d90176bb8c9168c3c71239f42c7
TheDarkzinho <abner.doubleq@gmail.com>
2023-05-10 14:01:01 -0300

    tcc(1.2)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import App from './App';
+import {Route, Routes, Redirect} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
