9e39921122eb6ead0183dd2154ccbe199a332f8c
Crisp do Acre <107727473+zCrispz@users.noreply.github.com>
2023-05-24 16:47:05 +0000

    Update 4 files

---
+++ src/Telas/conta/conta.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/styles.css";
+
+export default function Conta() {
+  return <h1>oi</h1>;
+}
--- src/rota.js
+++ src/rota.js
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Telas/home/home.js";
 import Login from "./Telas/login/Login";
 import Inicio from "./Telas/inicio.js";
+import Usuario from "./Telas/conta/conta.js";
 
-export default function rotas(){
-    return(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/Home" element={<Home/>}/>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/Inicio" element={<Inicio/>}/>
-          <Route path="*" element={<div> Não encontrado </div>}/>
-        </Routes>
-      </BrowserRouter>
-    )
+export default function rotas() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Inicio" element={<Inicio />} />
+        <Route path="/Usuario" element={<Usuario />} />
+        <Route path="*" element={<div> Não encontrado </div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
